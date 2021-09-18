d30834d37ba25bf94b4fff3f628e3dc294da3294
esmattoha <mondaldipu904@gmail.com>
2021-09-18 13:45:44 +0530

    Remove : Redundant code

--- test/order.test.js
+++ test/order.test.js
@@ -1,37 +1,54 @@
-const {
-  getMetaData,
-  getOrderItems,
-  productItem,
-} = require("../controllers/orderController");
-
-/**
- * 
- */
-test("Return a object after exicute the function", async () => {
+const orderController = require("../controllers/orderController");
+//
+test("Throw a 406 error", async () => {
   const req = {
-    body: function (device, model) {
-      return { device, model };
+    body: function (type, customerInfo, shippingAddress, service) {
+      return null;
     },
   };
-  expect(getMetaData(req)).resolves.toBe({});
+  // const data = await orderController.store(req, {}, () => {});
+   await expect(() =>orderController.store(req , {}, () =>{}) ).toThrow();
 });
-/**
- * 
- */
-test("return a object", async() => {
-  const req = {
-    body: function (product) {
-      return product;
-    },
-  };
 
-  expect(productItem(req)).resolves.toBe({});
-});
-/**
- * 
- */
-test("Return an array", async () => {
-  const items = ["60c02bb500c3478174c8e1ca", "60c02c2b00c3478174c8e1cc"];
-  const data = await getOrderItems(items);
-  expect(data).resolves.toBe([]);
-});
+// //
+// test("Should return a object", async () => {
+   
+//   const req = {
+//     body: function (
+//       type,
+//       items,
+//       productPrice,
+//       customerInfo,
+//       shippingAddress,
+//       billingAddress,
+//       customerNotes,
+//       service,
+//       appointmentTime
+//     ) {
+//       return {
+//         type,
+//         items,
+//         productPrice,
+//         customerInfo,
+//         shippingAddress,
+//         billingAddress,
+//         customerNotes,
+//         service,
+//         appointmentTime,
+//       };
+//     },
+//   };
+//   const data = await orderController.store(req, {}, () => {});
+//   expect(data).toBe("successfull");
+// });
+
+// // Delete
+// test("Should return a message", async()=>{
+//     const req = {
+//         params: function(id){
+//             return id ;
+//         }
+//     }
+//     const data = await orderController.delete(req, {}, () =>{});
+//     expect(data).toBe({ message: "Deleted" })
+// })
