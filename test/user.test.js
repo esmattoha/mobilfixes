c3a4143f04271a9bc92fe4a3aba7fd3bb305978b
esmattoha <mondaldipu904@gmail.com>
2021-09-21 09:03:34 +0530

    Test : User login & User/me

--- test/user.test.js
+++ test/user.test.js
@@ -1,64 +1,56 @@
 const request = require("supertest");
 const mongoose = require("mongoose");
-// const userRoute = require("../routers/userRoute");
 const app = require("../app");
+const crypto = require("crypto");
 
 let auth = {};
 
 // Connects to database power-rangers
 beforeAll(async () => {
-   await mongoose.connect(process.env.MONGODB_URL, {
+  await mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
   });
 });
 
+// describe("POST /user/signup", () => {
+//   test("It should return a status Code", async () => {
+//     const response = await request(app).post("/user/signup").send({
+//       name: "Dipu",
+//       email: "mondal@gmail.com",
+//       phone: "7718533953",
+//       password: "dipu_@",
+//     });
 
-describe("POST /user/signup", () => {
-  test("It should return a status Code", async () => {
-    const response = await request(app).post("/user/signup").send({
-      name: "Dipu",
-      email: "mondal@gmail.com",
-      phone: "7718533953",
+//     expect(response.statusCode).toBe(201);
+//   });
+// });
+
+describe("POST /user/login", () => {
+  test("It should return 200 status code", async () => {
+    const response = await request(app).post("/user/login").send({
+      email: "alex@gmail.com",
       password: "dipu_@",
     });
 
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.user.email).toBe("alex@gmail.com");
   });
 });
 
-describe("POST /user/login", ()=>{
-  test("It should return 200 status code", async()=>{
-    const response = await request(app).post("/user/login").send({
-      email:"alex@gmail.com",
-      password: "dipu_@"
-    })
+describe("POST /user/me", () => {
+  test("It should return 401 status code", async () => {
+    const response = await request(app).get("/user/me");
 
-    expect(response.statusCode).toBe(200);
-  })
-})
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toMatchObject({ message: "Unauthorized Request" });
+  });
+});
 
 
-beforeAll(async()=>{
-  const response = await request(app).post("/user/login").send({
-    email:"alex@gmail.com",
-    password: "dipu_@"
-  })
-
-  auth.token = response.body.token;
-})
-
-describe("POST /user/me", ()=>{
-  test("It should return 200 status code", async()=>{
-    const response = await request(app).get("/user/me").set("authorization", auth.token);
-
-    expect(response.statusCode).toBe(200);
-  })
-})
 
 //
 afterAll(async () => {
-     await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-    });
+  await mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
   });
-  
+});
